7312e8463681f4d48e94ceb45cad81be87cace67
Cácio Lucas <cclucas060901@gmail.com>
2022-10-04 09:39:48 -0300

    Corrigir referencia de imagens e estilo de forms

--- PetClinicSystem/apps/client/forms.py
+++ PetClinicSystem/apps/client/forms.py
@@ -1,20 +1,29 @@
-from unicodedata import name
 from django.forms import ModelForm, DateField, SelectDateWidget
 import datetime
-from .models import Client, Address, Pet
+from client.models import Client, Address, Pet
 
 class AddressForm(ModelForm):
     class Meta:
         model = Address
         fields = ('state', 'city', 'zip', 'district', 'street')
-
+    def __init__(self, *args, **kwargs):
+        super(AddressForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control' 
 class ClientForm(ModelForm):
     class Meta:
         model = Client
         fields = ['name', 'phone', 'email']
-
+    def __init__(self, *args, **kwargs):
+        super(ClientForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control' 
 class PetForm(ModelForm):
     birth_date = DateField(widget=SelectDateWidget(years=range(1900, datetime.date.today().year + 1)), initial=datetime.date.today, label='Data de nascimento')
     class Meta:
         model = Pet
         fields = ['name', 'breed', 'birth_date', 'owner']
+    def __init__(self, *args, **kwargs):
+        super(PetForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control' 
--- PetClinicSystem/apps/clinic/forms.py
+++ PetClinicSystem/apps/clinic/forms.py
@@ -1,21 +1,30 @@
 from django.forms import ModelForm, DateField, SelectDateWidget
-from .models import Drug, Veterinary, Appointment
+from clinic.models import Drug, Veterinary, Appointment
 import datetime
 
 class DrugForm(ModelForm):
     class Meta:
         model = Drug
         fields = ['name']
-
+    def __init__(self, *args, **kwargs):
+        super(DrugForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'   
 class VeterinaryForm(ModelForm):
     class Meta:
         model = Veterinary
         fields = ['name', 'email', 'phone']
-
+    def __init__(self, *args, **kwargs):
+        super(VeterinaryForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control' 
 class AppointmentForm(ModelForm):
     
     date = DateField(widget=SelectDateWidget(years=range(datetime.date.today().year, datetime.date.today().year + 2)), initial=datetime.date.today, label='Data do atendimento')
     class Meta:
         model = Appointment
         fields = ['date', 'description', 'diagnosis', 'drugs', 'pet', 'veterinary']
-
+    def __init__(self, *args, **kwargs):
+        super(AppointmentForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control' 
